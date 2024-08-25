4317cf3db8729ab0c932dc1ce3c808124916fb58
seank <seankenny6688@gmail.com>
2024-08-24 18:10:38 -0600

    First attempt at dividing into polygons

--- src/image-data-to-polygons.ts
+++ src/image-data-to-polygons.ts
@@ -21,55 +21,152 @@ interface ConsolidateImageDataIntoPolygonsOutput<TData extends Record<string, un
 export const consolidateImageDataIntoPolygons = <TData extends Record<string, unknown>>(
   input: ConsolidateImageDataIntoPolygonsInput<TData>
 ): ConsolidateImageDataIntoPolygonsOutput<TData> => {
-  const polygons: Array<Polygon<TData>> = [
-    {
-      type: "Polygon",
-      coordinates: [
-        [
-          [40.0, 40.0],
-          [20.0, 45.0],
-          [45.0, 30.0],
-          [40.0, 40.0]
-        ]
-      ],
-      properties: {
-        colourHexCode: '#FF00FF',
-        data: null
-      }
-    },
-    {
-      type: "Polygon",
-      coordinates: [
-        [
-          [20.0, 35.0],
-          [10.0, 30.0],
-          [10.0, 10.0],
-          [30.0, 5.0],
-          [45.0, 20.0],
-          [20.0, 35.0]
-        ]
-      ],
-      properties: {
-        colourHexCode: '#00FFFF',
-        data: null
-      }
-    },
-    {
-      type: "Polygon",
-      coordinates: [
-        [
-          [30.0, 20.0],
-          [20.0, 15.0],
-          [20.0, 25.0],
-          [30.0, 20.0]
-        ]
-      ],
-      properties: {
-        colourHexCode: '#0000FF',
-        data: null
-      }
+  if(input.imageData.length === 0) {
+    return { polygons: [] };
+  }
+
+  const imageDataWithProcessedFlag = input.imageData
+    .map((row) => row
+      .map((cell) => ({ red: cell.red, green: cell.green, blue: cell.blue, processedFlag: false }))
+    )
+  
+  const totalRowCount = imageDataWithProcessedFlag.length;
+  const totalColCount = imageDataWithProcessedFlag[0].length;
+  
+  let currentRowIndex = 0;
+  let currentColIndex = 0;
+  let polygons: Array<Polygon<TData>> = []
+
+  while(currentRowIndex < totalRowCount) {
+    while(currentColIndex < totalColCount) {
+      const starterCellForPolygon = imageDataWithProcessedFlag[currentRowIndex][currentColIndex];
+
+      let cellProcessingQueue = [{
+        red: starterCellForPolygon.red,
+        green: starterCellForPolygon.green,
+        blue: starterCellForPolygon.blue,
+        rowIndex: currentRowIndex,
+        colIndex: currentColIndex,
+        processedFlag: starterCellForPolygon.processedFlag
+      }];
+      cellProcessingQueue = cellProcessingQueue.filter((element) => !element.processedFlag);
+      const cellsForPolygon: Array<{
+        red: number;
+        green: number;
+        blue: number;
+        rowIndex: number;
+        colIndex: number;
+      }> = []
+
+      while( cellProcessingQueue.length > 0) {
+        const currentCell = cellProcessingQueue[0];
+        console.log('currentCell.rowIndex', currentCell.rowIndex);
+        console.log('currentCell.colIndex', currentCell.colIndex);
+        cellProcessingQueue.shift();
+        
+        if(currentCell.processedFlag === true) {
+          continue;
+        }
+
+        cellsForPolygon.push({
+          red: currentCell.red,
+          green: currentCell.green,
+          blue: currentCell.blue,
+          rowIndex: currentCell.rowIndex,
+          colIndex: currentCell.colIndex,
+        });
+
+        imageDataWithProcessedFlag[currentCell.rowIndex][currentCell.colIndex] = {
+          red: currentCell.red,
+          green: currentCell.green,
+          blue: currentCell.blue,
+          processedFlag: true
+        }
+
+        if(currentCell.colIndex !== 0) {
+          const cellToTheLeft = imageDataWithProcessedFlag[currentCell.rowIndex][currentCell.colIndex - 1];
+
+          if(
+            !cellToTheLeft.processedFlag
+            && cellToTheLeft.red === currentCell.red
+            && cellToTheLeft.green === currentCell.green
+            && cellToTheLeft.blue === currentCell.blue
+          ) {
+            cellProcessingQueue.push({
+              red: cellToTheLeft.red,
+              green: cellToTheLeft.green,
+              blue: cellToTheLeft.blue,
+              rowIndex: currentCell.rowIndex,
+              colIndex: currentCell.colIndex - 1,
+              processedFlag: cellToTheLeft.processedFlag
+            })
+          }
+        }
+        if(currentCell.colIndex < (totalColCount - 1)) {
+          const cellToTheRight = imageDataWithProcessedFlag[currentCell.rowIndex][currentCell.colIndex + 1];
+
+          if(
+            !cellToTheRight.processedFlag
+            && cellToTheRight.red === currentCell.red
+            && cellToTheRight.green === currentCell.green
+            && cellToTheRight.blue === currentCell.blue
+          ) {
+            cellProcessingQueue.push({
+              red: cellToTheRight.red,
+              green: cellToTheRight.green,
+              blue: cellToTheRight.blue,
+              rowIndex: currentCell.rowIndex,
+              colIndex: currentCell.colIndex + 1,
+              processedFlag: cellToTheRight.processedFlag
+            })
+          }
+        }
+        if(currentCell.rowIndex !== 0) {
+          const cellAbove = imageDataWithProcessedFlag[currentCell.rowIndex - 1][currentCell.colIndex];
+
+          if(
+            !cellAbove.processedFlag
+            && cellAbove.red === currentCell.red
+            && cellAbove.green === currentCell.green
+            && cellAbove.blue === currentCell.blue
+          ) {
+            cellProcessingQueue.push({
+              red: cellAbove.red,
+              green: cellAbove.green,
+              blue: cellAbove.blue,
+              rowIndex: currentCell.rowIndex - 1,
+              colIndex: currentCell.colIndex,
+              processedFlag: cellAbove.processedFlag
+            })
+          }
+        }
+        if(currentCell.rowIndex < (totalRowCount - 1)) {
+          const cellBelow = imageDataWithProcessedFlag[currentCell.rowIndex + 1][currentCell.colIndex];
+
+          if(
+            !cellBelow.processedFlag
+            && cellBelow.red === currentCell.red
+            && cellBelow.green === currentCell.green
+            && cellBelow.blue === currentCell.blue
+          ) {
+            cellProcessingQueue.push({
+              red: cellBelow.red,
+              green: cellBelow.green,
+              blue: cellBelow.blue,
+              rowIndex: currentCell.rowIndex + 1,
+              colIndex: currentCell.colIndex,
+              processedFlag: cellBelow.processedFlag
+            })
+          }
+        }
+      }  
+
+      currentColIndex++;
     }
-  ];
+
+    currentColIndex = 0;
+    currentRowIndex++;
+  }
 
   return {
     polygons
--- test/index.test.ts
+++ test/index.test.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { readFileSync } from 'fs';
 import { writeFile } from 'fs/promises';
 
-test('the exportColouredBitmapToGeoJSONPolygons function is callable.', { timeout: 50000 }, async() => {
+test('exportColouredBitmapToGeoJSONPolygons should produce the right output for provinces.bmp.', { timeout: 50000 }, async() => {
   const input = {
     inputFilePath: path.join(
       __dirname,
@@ -49,6 +49,63 @@ test('the exportColouredBitmapToGeoJSONPolygons function is callable.', { timeou
         compressionType: "BI_RGB",
         horizontalPixelsPerMetre: 0,
         imageCompressedSizeBytes: 2,
+        infoHeaderSizeBytes: 0,
+        numberOfColoursUsed: 0,
+        numberOfImportantColours: 0,
+        numberOfPlanes: 0,
+        verticalPixelsPerMetre: 0
+      },
+      colourMap: {}
+    },
+    outputGeoJSON: expectedOutputGeoJSON
+  });
+});
+
+test('exportColouredBitmapToGeoJSONPolygons should produce the right output for bmp-24.bmp.', { timeout: 50000 }, async() => {
+  const input = {
+    inputFilePath: path.join(
+      __dirname,
+      'sample-inputs',
+      'bmp-24.bmp'
+    ),
+    bitmapWidthPx: 200,
+    bitmapHeightPx: 200,
+    colourToPropertiesMap: {}
+  }
+  const output = await exportColouredBitmapToGeoJSONPolygons(input);
+
+  const pathToExpectedOutputGeoJSONFile = path.join(
+    __dirname,
+    'expected-outputs',
+    'bmp-24-expected-geojson-output.json'
+  );
+  const expectedOutputGeoJSON = JSON.parse(readFileSync(pathToExpectedOutputGeoJSONFile).toString());
+
+  const outputGeoJSONToCreateArtifact = JSON.stringify(output.outputGeoJSON);
+
+  const pathToWriteArtifactTo = path.join(
+    __dirname,
+    'artifacts',
+    'bmp-24-geojson-artifact.json'
+  );
+
+  await writeFile(pathToWriteArtifactTo, outputGeoJSONToCreateArtifact, 'utf8');
+
+  expect(output).toStrictEqual({
+    bmpFileMetadata: {
+      header: {
+        signature: 'BM',
+        fileSizeBytes: 0,
+        reservedField: '00000000',
+        dataOffsetBytes: 0 
+      },
+      infoHeader: {
+        bitsPerPixel: "ZERO",
+        bmpHeightPx: 0,
+        bmpWidthPx: 0,
+        compressionType: "BI_RGB",
+        horizontalPixelsPerMetre: 0,
+        imageCompressedSizeBytes: 0,
         infoHeaderSizeBytes: 0,
         numberOfColoursUsed: 0,
         numberOfImportantColours: 0,
